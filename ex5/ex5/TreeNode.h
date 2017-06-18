aa99df3afcce351ee7856542ad8fa78e126bd0f2
Ilay Pilo <ilay.pilo@gmail.com>
2017-06-18 10:50:48 +0300

    ex 5

--- ex5/ex5/TreeNode.h
+++ ex5/ex5/TreeNode.h
@@ -16,23 +16,76 @@ private:
 	vector< TreeNode<T>* > _children;
 
 public:
+	// children constructor
+	TreeNode(TreeNode<T>* parent,T* data)
+	{
+		_parent = parent;
+		_data = data;
+	}
+	// root constructor
+	TreeNode(T* data)
+	{
+		_parent = nullptr;
+		_data = data;
+	}
+	// destructor
+	~TreeNode()
+	{
+		
+	}
 
-	TreeNode(TreeNode<T>* parent,T* data);
-	TreeNode(T* data);
-	~TreeNode();
+	T* getData() const
+	{
+		return _data;
+	}
+	void setData(T* data)
+	{
+		_data = data;
+	}
+	void addChild(T* data)
+	{
+		_children->push_back(new TreeNode<T>(this, data));
+	}
 
-	T* getData() const;
-	void setData(T* data);
-	void addChild(T* data);
-	void addChild(TreeNode<T>* newChild);
-	void removeChild(); //removes the nodes last entered child or if no children - does nothing
-	TreeNode<T>* findChild(T* data) const; // returns ptr or NULL
+	void addChild(TreeNode<T>* newChild)
+	{
+		newChild->setParent(this);
+		_children->push_back(newChild);
+	}
+	//removes the nodes last entered child or if no children - does nothing
+	void removeChild() 
+	{ 
+		if(_children->size())
+		{
+			_children->erase(_children->end());
+		}
+	}
+	// returns ptr or NULL
+	TreeNode<T>* findChild(T* data) const
+	{
+		for(auto i=0; i<_children->size();i++)
+		{
+			if(_children->at(i)->_data == data)
+			{
+				return _children->at(i);
+			}
+		}
+	}
 	int findChildIndx(T* data) const; // returns childs indx or -1
 	TreeNode<T>* getChild(int indx) const;
 	TreeNode<T>* getNextChild() const;
 	TreeNode<T>* getPrevChild() const;
-	TreeNode<T>* getParent() const;
-	void setParent(TreeNode<T>* parent);
-	int getNumChildren() const;
+	TreeNode<T>* getParent() const
+	{
+		return _parent;
+	}
+	void setParent(TreeNode<T>* parent)
+	{
+		_parent = parent;
+	}
+	int getNumChildren() const
+	{
+		return _children->size();
+	}
 
 };
