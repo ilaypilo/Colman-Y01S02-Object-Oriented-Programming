7e4ca63da54e73c3c4de80c2e4104f5881d2b958
Ilay Pilo <ilay.pilo@gmail.com>
2017-06-27 00:51:11 +0300

    implament Rectangle methods in the cpp

--- ex6/ex6/Rectangle.cpp
+++ ex6/ex6/Rectangle.cpp
@@ -1,1 +1,54 @@
-//TODO implament Rectangle methods here
+#include "Rectangle.h"
+
+vector<Rectangle> TreeSplitRectangle::operator()(const Rectangle& rect) const
+{
+	// Rectangle: int x, int y, int width, int height
+	auto wh = rect.width() / 2;
+	auto hh = rect.height() / 2;
+	vector<Rectangle> splited;
+	Rectangle q0(rect.x() + wh, rect.y(), wh, hh);
+	Rectangle q1(rect.x(), rect.y(), wh, hh);
+	Rectangle q2(rect.x(), rect.y() + hh, wh, hh);
+	Rectangle q3(rect.x() + wh, rect.y() + hh, wh, hh);
+	splited.push_back(q0);
+	splited.push_back(q1);
+	splited.push_back(q2);
+	splited.push_back(q3);
+
+	return splited;
+}
+
+int TreeIndexRectangle::operator()(const Rectangle& outer, const Rectangle& inner) const
+{
+
+	auto q = -1;
+	if (inner.y() + inner.height() > (outer.y() + outer.height()) ||
+		inner.x() + inner.width() > (outer.x() + outer.width()))
+	{
+		q = -1;
+	}
+
+	//check x
+	if (inner.x() < outer.x() + (outer.width() / 2))
+	{
+		q = 1;
+	}
+	else
+	{
+		q = 0;
+	}
+	//check y
+	if (inner.y() > outer.y() + (outer.height() / 2))
+	{
+		switch (q)
+		{
+		case 0:
+			q = 3;
+			break;
+		case 1:
+			q = 2;
+			break;
+		}
+	}
+	return q;
+}
--- ex6/ex6/Rectangle.h
+++ ex6/ex6/Rectangle.h
@@ -29,10 +29,6 @@ public:
 	{
 		return (_x==rect.x() && _y == rect.y() && _height == rect.height() && _width == rect.width() );
 	}
-	//bool operator!=(const Rectangle &rect) const
-	//{
-	//	return (_x != rect.x() || _y != rect.y() || _height != rect.height() || _width != rect.width());
-	//}
 
 private:
     int _height, _width;
@@ -44,59 +40,10 @@ private:
 
 struct TreeSplitRectangle
 {
-	vector<Rectangle> operator()(const Rectangle& rect) const 
-	{
-		// Rectangle: int x, int y, int width, int height
-		auto wh = rect.width() / 2;
-		auto hh = rect.height() / 2;
-		vector<Rectangle> splited;
-		Rectangle q0(rect.x()+ wh, rect.y(), wh, hh);
-		Rectangle q1(rect.x(), rect.y(), wh, hh);
-		Rectangle q2(rect.x(), rect.y()+ hh, wh, hh);
-		Rectangle q3(rect.x()+ wh, rect.y()+ hh, wh, hh);
-		splited.push_back(q0);
-		splited.push_back(q1);
-		splited.push_back(q2);
-		splited.push_back(q3);
-
-		return splited;
-	}
+	vector<Rectangle> operator()(const Rectangle& rect) const;
 };
 
 struct TreeIndexRectangle
 {
-	int operator()(const Rectangle& outer, const Rectangle& inner) const 
-	{
-
-		auto q = -1;
-		if (inner.y() + inner.height() > (outer.y() + outer.height()) ||
-			inner.x() + inner.width() > (outer.x() + outer.width()) )
-		{
-			q = -1;
-		}
-
-		//check x
-		if (inner.x() < outer.x() + (outer.width() / 2))
-		{
-			q = 1;
-		}
-		else
-		{
-			q = 0;
-		}
-		//check y
-		if (inner.y() > outer.y() + (outer.height() / 2))
-		{
-			switch (q) 
-			{
-			case 0:
-				q = 3;
-				break;
-			case 1:
-				q = 2;
-				break;
-			}
-		}
-		return q;
-	}
+	int operator()(const Rectangle& outer, const Rectangle& inner) const;
 };
